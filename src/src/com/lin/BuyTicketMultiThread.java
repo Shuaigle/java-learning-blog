0b0ed1029df88de4dfd6ced31f5f30181a577a93
shuaigle <layard3@gmail.com>
2022-06-21 12:53:09 +0800

    add not secure ticket-buying thread

---
+++ src/src/com/lin/BuyTicketMultiThread.java
@@ -0,0 +1,30 @@
+package com.lin;
+
+// Not secure
+public class BuyTicketMultiThread implements Runnable{
+
+    private int ticketNums = 20;
+
+    @Override
+    public void run() {
+
+        while (ticketNums > 0) {
+//            try {
+//                Thread.sleep(10);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+
+            System.out.println(Thread.currentThread().getName() + " get " + ticketNums-- + "th ticket");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        BuyTicketMultiThread ticket = new BuyTicketMultiThread();
+
+        new Thread(ticket, "ace").start();
+        new Thread(ticket, "bebe").start();
+        new Thread(ticket, "cat").start();
+    }
+}
